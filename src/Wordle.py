9db5295e0e74a5338e52f695228af742f5c1813e
Ben Anderson <31827732+benton-anderson@users.noreply.github.com>
2022-01-24 00:45:50 -0600

    Big changes in understanding to probability and total green probability

--- src/Wordle.py
+++ src/Wordle.py
@@ -5,7 +5,8 @@ with open(r'..\data\processed\wordle_words.json') as infile:
 	word_list = json.load(infile)
 
 class Wordle:
-    def __init__(self, word=None, n_guesses=6, n_letters=5, all_words=word_list):
+    def __init__(self, word=None, n_guesses=6, n_letters=5, all_words=word_list, ben_mode=False):
+        self.ben_mode = ben_mode
         if word == None:
             self.word = random.sample(set(all_words), 1)[0]
         else:
@@ -15,7 +16,7 @@ class Wordle:
         self.guess_counter = n_guesses
         self.greens = [None] * self.n_letters 
         self.current_yellows = [None] * self.n_letters
-        self.yellows = {i: [] for i in range(self.n_letters)}
+        self.yellows_pos = {i: [] for i in range(self.n_letters)}
         self.guessed_words = []
         self.all_words = [word.upper() for word in all_words]
         self.absent_letters = []
@@ -31,11 +32,11 @@ class Wordle:
         # need to add a check for not doing anything if the word is already guessed correctly
         guess = guess.upper()
         self.state = None
-        self.current_yellows = [None]* self.n_letters # yellows have to reset each time
-        self.greens = [None] * self.n_letters
+        self.current_yellows = [None] * self.n_letters 
+        self.current_greens = [None] * self.n_letters
         self.tmp_qstate = [0] * self.n_letters
         
-        print('guessed: ', guess)
+        # if not self.ben_mode: print('guessed: ', guess)
         if not len(guess) == self.n_letters:
             raise ValueError('wrong word length')
         if guess not in self.all_words:
@@ -57,9 +58,13 @@ class Wordle:
                 print('victory! word is: ' + self.word) 
                 grn_sum = sum([x!=None for x in self.greens])
                 yel_sum = sum([x!=None for x in self.current_yellows])
-                return grn_sum, yel_sum
+                
+                if self.ben_mode:
+                    return self.ben_mode_return()
+                else:
+                    return grn_sum, yel_sum
             if c_g in self.word and c_g != c_w:
-                self.yellows[i].append(c_g)
+                self.yellows_pos[i].append(c_g)
                 self.current_yellows[i] = c_g
                 if c_g not in self.yellowlist:
                     self.yellowlist += c_g
@@ -74,11 +79,21 @@ class Wordle:
         if self.guess_counter == 0:
             self.state=True
             self.win = False
-            print("YOU LOSE-TOO MANY GUESSES")
+            if not self.ben_mode: print("YOU LOSE-TOO MANY GUESSES")
+            if self.ben_mode:
+                return self.ben_mode_return()
+            else:
+                return grn_sum, yel_sum # return number of greens and number of yellows
+        if not self.ben_mode: 
+            # print('Greens:' + str(self.greens) + 
+            #       '\nYellows: ' + str(self.yellows_pos) + 
+            #       '\nGuesses left: ' + str(self.guess_counter))
+            pass
+			
+        if self.ben_mode:
+            return self.ben_mode_return()
+        else:
             return grn_sum, yel_sum # return number of greens and number of yellows
-        print('Greens:' + str(self.greens) + 
-              '\nYellows: ' + str(self.yellows) + 
-              '\nGuesses left: ' + str(self.guess_counter))
-
-        return grn_sum, yel_sum # return number of greens and number of yellows
-        
+    
+    def ben_mode_return(self):
+        return self.greens, self.yellowlist, self.absent_letters
